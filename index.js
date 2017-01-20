0819cd2e84f64da3afdd8e229429195439550686
Erika Tarazona <tarazonaerika@gmail.com>
2017-01-20 04:37:35 +0100

    Replace s/app/host

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ module.exports = {
   name: 'ember-cli-g-maps',
 
   // Include Gmaps code in consuming app
-  included: function() {
-    this._super.included(...arguments);
+  included: function(app) {
+    this._super.included(app);
 
-    var app;
+    var host;
 
     // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
     // use that.
     if (typeof this._findHost === 'function') {
-      app = this._findHost();
+      host = this._findHost();
     } else {
       // Otherwise, we'll use this implementation borrowed from the _findHost()
       // method in ember-cli.
       var current = this;
       do {
-        app = current.app || app;
+        host = current.app || host;
       } while (current.parent.parent && (current = current.parent));
     }
 
-    app.import(app.bowerDirectory + '/gmaps-for-apps/gmaps.js');
+    host.import(app.bowerDirectory + '/gmaps-for-apps/gmaps.js');
   },
 
   // Request Google Maps script in consuming app
